377ea35870d1fa0017b67f21c4063bb48c40c8ec
ehsan2793 <ehsan2793@gmail.com>
2023-08-26 12:49:26 -0700

    start

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,18 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            BottomCardView()
+                .frame(width: 280, height: 220)
+                .background(Color.orange)
+                .shadow(radius: 20)
+                .cornerRadius(20)
+                .offset(x: 0, y: -40)
+            
+            BottomCardView()
+                .frame(width: 300, height: 220)
+                .background(Color.blue)
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: -20)
+
+            CardView()
+        } //: ZSTACK
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        Spacer()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    Text("Certification")
+                        .foregroundColor(Color("accent"))
+                } //: vstack
+                Spacer()
+                Image("Logo1")
+            } //: HSTACK
+            .padding(20)
+            
+            Image("Card1")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 300, height: 110, alignment: .top)
+        } //: VSTACK
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
     }
 }
