7b285c5b468e7e2a50373c9472ed1851d249bcda
ehsan2793 <ehsan2793@gmail.com>
2023-08-30 19:53:23 -0700

    added animation

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -7,36 +7,44 @@
 
 import SwiftUI
 struct ContentView: View {
+    @State var show: Bool = false
     var body: some View {
         ZStack {
             TitleView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
 
             BackCardView()
-                .background(Color("card4"))
+                .background(show ? Color("card3") : Color("card4"))
                 .shadow(radius: 20)
                 .cornerRadius(20)
-                .offset(x: 0, y: -40)
+                .offset(x: 0, y: show ? -400 : -40)
                 .scaleEffect(0.90)
-                .rotationEffect(Angle(degrees: 10))
+                .rotationEffect(Angle(degrees: show ? 0 : 10))
                 .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0.0, z: 0))
                 .blendMode(.hardLight)
+//                .animation(.easeIn(duration: 0.4), value: show)
 
             BackCardView()
-                .background(Color("card3"))
+                .background(show ? Color("card4") : Color("card3"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -20)
+                .offset(x: 0, y: show ? -200 : -20)
                 .scaleEffect(0.95)
-                .rotationEffect(Angle(degrees: 5))
+                .rotationEffect(Angle(degrees: show ? 0 : 5))
                 .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0.0, z: 0))
                 .blendMode(.hardLight)
 
             CardView()
                 .blendMode(.hardLight)
+                .onTapGesture {
+                    withAnimation(Animation.spring()) {
+                        show.toggle()
+                    }
+                  
+                }
 
             BottomCardView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
         } //: ZSTACK
     }
 }
@@ -105,7 +113,7 @@ struct BottomCardView: View {
     var body: some View {
         VStack(spacing: 20) {
             RoundedRectangle(cornerRadius: 3)
-                .frame(width: 40, height: 5)
+                .frame(width: 60, height: 5)
                 .opacity(0.1)
             
             Text("This certificate is proof that Meng To has achieved the UI Design course with approval from a Design+Code instructor.")
